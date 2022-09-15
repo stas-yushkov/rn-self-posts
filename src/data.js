c4b76c8a721521504ae1efb8c2b40d38dfa51ab3
stas-yushkov <stas.yushkov@gmail.com>
2022-09-15 05:36:44 +0300

    add mock DATA

---
+++ src/data.js
@@ -0,0 +1,86 @@
+export const DATA = [
+  {
+    id: '0',
+    img: 'https://unsplash.com/photos/yC-Yzbqy7PY',
+    text: 'Alejandro Escamilla',
+    date: new Date().toJSON(),
+    booked: true
+  },
+  {
+    id: '1',
+    img: 'https://unsplash.com/photos/LNRyGwIJr5c',
+    text: 'Alejandro Escamilla',
+    date: new Date().toJSON(),
+    booked: true
+  },
+  {
+    id: '10',
+    img: 'https://unsplash.com/photos/6J--NXulQCs',
+    text: 'Paul Jarvis',
+    date: new Date().toJSON(),
+    booked: false
+  },
+  {
+    id: '100',
+    img: 'https://unsplash.com/photos/pwaaqfoMibI',
+    text: 'Tina Rataj',
+    date: new Date().toJSON(),
+    booked: false
+  },
+  {
+    id: '1000',
+    img: 'https://unsplash.com/photos/6cY-FvMlmkQ',
+    text: 'Lukas Budimaier',
+    date: new Date().toJSON(),
+    booked: true
+  },
+  {
+    id: '1001',
+    img: 'https://unsplash.com/photos/1DkWWN1dr-s',
+    text: 'Danielle MacInnes',
+    date: new Date().toJSON(),
+    booked: false
+  },
+  {
+    id: '1002',
+    img: 'https://unsplash.com/photos/6-jTZysYY_U',
+    text: 'NASA',
+    date: new Date().toJSON(),
+    booked: false
+  },
+  {
+    id: '1003',
+    img: 'https://unsplash.com/photos/GYumuBnTqKc',
+    text: 'E+N Photographies',
+    date: new Date().toJSON(),
+    booked: true
+  },
+  {
+    id: '1004',
+    img: 'https://unsplash.com/photos/SSxIGsySh8o',
+    text: 'Greg Rakozy',
+    date: new Date().toJSON(),
+    booked: true
+  },
+  {
+    id: '1005',
+    img: 'https://unsplash.com/photos/tBtuxtLvAZs',
+    text: 'Matthew Wiebe',
+    date: new Date().toJSON(),
+    booked: false
+  },
+  {
+    id: '1006',
+    img: 'https://unsplash.com/photos/-wWRHIUklxM',
+    text: 'Vladimir Kudinov',
+    date: new Date().toJSON(),
+    booked: false
+  },
+  {
+    id: '1008',
+    img: 'https://unsplash.com/photos/5L4XAgMSno0',
+    text: 'Benjamin Combs',
+    date: new Date().toJSON(),
+    booked: false
+  },
+];
