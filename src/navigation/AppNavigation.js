50550e6d335c563599152ff073ec5b523c1413b8
stas-yushkov <stas.yushkov@gmail.com>
2022-09-15 05:21:57 +0300

    add Theme control to screens (defaultNavigationOptions didn't work,  but this useEffect need to be fixed in react-navigation v6)

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -3,26 +3,26 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
-
-import { COLORS } from '../constants/colors';
-
-// CreateScreen
-// AboutScreen
-// BookmarkedScreen
+import { CreateScreen } from '../screens/CreateScreen';
+import { AboutScreen } from '../screens/AboutScreen';
+import { BookmarkedScreen } from '../screens/BookmarkedScreen';
 
 const PostNavigator = createStackNavigator({
   Main: MainScreen,
+  Create: CreateScreen,
+  About: AboutScreen,
+  Bookmarked: BookmarkedScreen,
   Post: {
     screen: PostScreen
   }
 },
   {
     initialRouteName: 'Main',
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: COLORS['dark'].navbarBgColor,
-      },
-      headerTintColor: COLORS['dark'].accentColor,
+    defaultNavigationOptions: {//need to be fixed in react-navigation v6
+      // headerStyle: {
+      //   backgroundColor: COLORS['dark'].navbarBgColor,
+      // },
+      // headerTintColor: COLORS['dark'].accentColor,
     }
   }
 )
